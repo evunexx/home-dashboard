d70a987abfbba020c16c08df6be05a0ff70fcc03
bastiSW <sebastian.weist@perfact.de>
2020-12-17 19:32:30 +0100

    Change Layout

--- dashboard/static/clock.js
+++ dashboard/static/clock.js
@@ -1,24 +1,34 @@
 function realtimedateandclock() {
     var clock = new Date();
-
     var hours = clock.getHours();
     var minutes = clock.getMinutes();
-    var day = clock.getDay();
+    var date = clock.getDate();
+    var month = getmonthname();
+    var dayname = getdayname();
+
     function getmonthname() {
         var t = new Date();
         const monthNames = ["Januar", "Februar", "März", "April", "Mai", "Juni",
         "Juli", "August", "September", "Oktober", "November", "Dezember"];
         var month = monthNames[t.getMonth()];
-        console.log(month)
         return month;
     }
-    var month = getmonthname();
 
-    var htmldate = day + "." + month
-    $( ".date" ).html(htmldate);
+    function getdayname() {
+        var t = new Date();
+        const dayNames = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag",
+        "Samstag", "Sonntag"];
+        var day = dayNames[t.getDay()];
+        return day;
+    }
 
-    var htmltime = hours + ":" + minutes
-    $(".time").html(htmltime);
+
+
+    var htmldate = dayname + ", " + date + "." + month;
+    $( "#date" ).html(htmldate);
+
+    var htmltime = hours + ":" + minutes;
+    $("#time").html(htmltime);
 
 }
 $(document).ready(function() {
