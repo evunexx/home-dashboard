e664941020f73672c3e1a1eb39ab3bb657a42095
evunexx <s.weist@me.com>
2020-12-23 12:35:42 +0100

    adjust garbage pickup to return if date is given

--- dashboard/routes.py
+++ dashboard/routes.py
@@ -7,6 +7,7 @@ from csv_ical import Convert
 import csv
 import re
 import json
+import os
 
 # Config files this has to be excluded
 
@@ -34,6 +35,7 @@ def bin():
 
     now = datetime.datetime.now()
     year = now.year
+    next_year = year + 1
     month = now.strftime("%m")
 
     url = (
@@ -41,10 +43,15 @@ def bin():
         % (month, year, month)
     )
 
-    ical_file = requests.get(url)
+    url_year = (
+        "https://swk.herford.de/output/abfall_export.php?csv_export=1&mode=vcal&ort=393.9&strasse=395.501.1&vtyp=2&vMo=01&vJ=%s&bMo=12"
+        % (year)
+    )
+
+    ical_file_current_year = requests.get(url_year)
 
     ical = open("ical3.ics", "wb")
-    ical.write(ical_file.content)
+    ical.write(ical_file_current_year.content)
     ical.close()
 
     csv_file = convert_ics_to_csv(ical.name)
@@ -66,11 +73,27 @@ def bin():
             yellow.append(row[1])
             blue.append(row[1])
 
+    LOG.warning(green)
+    LOG.warning(grey)
+    LOG.warning(yellow)
     summary = {}
-    summary["green"] = next_closest_date(green)
-    summary["blue"] = next_closest_date(blue)
-    summary["yellow"] = next_closest_date(yellow)
-    summary["grey"] = next_closest_date(grey)
+
+    date_green = next_closest_date(green)
+    date_blue = next_closest_date(blue)
+    date_yellow = next_closest_date(yellow)
+    date_grey = next_closest_date(grey)
+
+    if date_green:
+        summary["green"] = date_green
+
+    if date_blue:
+        summary["blue"] = date_blue
+
+    if date_yellow:
+        summary["yellow"] = date_yellow
+
+    if date_grey:
+        summary["grey"] = date_grey
 
     summary_json = jsonify(summary)
 
