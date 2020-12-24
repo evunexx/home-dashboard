157a56263ef5d67abd9a0a2136bae05cea9ea4c9
evunexx <s.weist@me.com>
2020-12-24 10:40:49 +0100

    Adjust config

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -3,7 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # Geo-Location --> Herford
-app.config['Latitude'] = '52.1152245'
-app.config['Longitude'] = '8.6711118'
+app.config["Latitude"] = "52.1152245"
+app.config["Longitude"] = "8.6711118"
+app.config["APIKEY"] = "124461e37ae9cb09ac472ae7cdf6caf0"
+app.config["CITY"] = "Elverdissen"
 
 import dashboard.routes
